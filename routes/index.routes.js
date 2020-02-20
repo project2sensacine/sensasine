3aac61d215e459ca3a3877132be2d6952af819cd
Carlos González <cargonamt@gmail.com>
2020-02-20 17:33:36 +0100

    maqueting

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,23 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const axios = require('axios')
-const axiosApp = axios.create({ baseURL: `https://api.themoviedb.org/3` })
+const axios = require("axios");
+const axiosApp = axios.create({ baseURL: `https://api.themoviedb.org/3` });
 
 /* GET home page */
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
+  const popularMovies = axiosApp.get(
+    `/movie/popular?api_key=${process.env.movieAPI}&language=en-US&page=1`
+  );
+  const upcomingMovies = axiosApp.get(
+    `/movie/upcoming?api_key=${process.env.movieAPI}&language=en-US&page=1`
+  );
+  const bestMovies = axiosApp.get(
+    `/movie/top_rated?api_key=${process.env.movieAPI}&language=en-US&page=1`
+  );
 
-  const popularMovies = axiosApp.get(`/movie/popular?api_key=${process.env.movieAPI}&language=en-US&page=1`)
-  const upcomingMovies = axiosApp.get(`/movie/upcoming?api_key=${process.env.movieAPI}&language=en-US&page=1`)
-
-
-  Promise.all([popularMovies, upcomingMovies])
+  Promise.all([popularMovies, upcomingMovies, bestMovies])
     .then(promiseResult => {
-      res.render('index', { popular: promiseResult[0].data.results, upcoming: promiseResult[1].data.results })
+      res.render("index", {
+        popular: promiseResult[0].data.results,
+        upcoming: promiseResult[1].data.results,
+        top_rated: promiseResult[2].data.results
+      });
     })
-    .catch(err => console.log(err))
-
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
