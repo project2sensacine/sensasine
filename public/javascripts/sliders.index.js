3a6ccb3fb6eb1e889786a9b52d2ba96c52aa4220
Carlos González <cargonamt@gmail.com>
2020-02-21 11:17:41 +0100

    pages

--- public/javascripts/sliders.index.js
+++ public/javascripts/sliders.index.js
@@ -1,34 +1,28 @@
-console.log("entra en el script Sliders")
+console.log("entra en el script Sliders");
 
-if (document.querySelectorAll('.glide').length) {
-
-  new Glide('.glide', {
-    type: 'slider',
+if (document.querySelectorAll(".glide").length) {
+  new Glide(".glide", {
+    type: "carousel",
     startAt: 0,
     perView: 6
   }).mount();
 }
 
-
-if (document.querySelectorAll('.glide2').length) {
-
-  new Glide('.glide2', {
-    type: 'slider',
+if (document.querySelectorAll(".glide2").length) {
+  new Glide(".glide2", {
+    type: "carousel",
     startAt: 7,
     perView: 6
   }).mount();
 }
 
-
-if (document.querySelectorAll('.glide3').length) {
-
-  new Glide('.glide3', {
-    type: 'slider',
+if (document.querySelectorAll(".glide3").length) {
+  new Glide(".glide3", {
+    type: "carousel",
     startAt: 0,
     perView: 6
   }).mount();
 }
-
 
 // if (document.querySelectorAll('.glide1').length) {
 
--- public/javascripts/sliders.profile.js
+++ public/javascripts/sliders.profile.js
@@ -1,34 +1,28 @@
-console.log("entra en el script Sliders")
+console.log("entra en el script Sliders");
 
-if (document.querySelectorAll('#glide1').length) {
-
-  new Glide('#glide1', {
-    type: 'slider',
+if (document.querySelectorAll("#glide1").length) {
+  new Glide("#glide1", {
+    type: "carousel",
     startAt: 0,
     perView: 3
   }).mount();
 }
 
-
-if (document.querySelectorAll('#glide2').length) {
-
-  new Glide('.glide2', {
-    type: 'slider',
+if (document.querySelectorAll("#glide2").length) {
+  new Glide(".glide2", {
+    type: "carousel",
     startAt: 0,
     perView: 3
   }).mount();
 }
 
-
-if (document.querySelectorAll('#glide3').length) {
-
-  new Glide('.glide3', {
-    type: 'slider',
+if (document.querySelectorAll("#glide3").length) {
+  new Glide(".glide3", {
+    type: "carousel",
     startAt: 0,
     perView: 6
   }).mount();
 }
-
 
 // if (document.querySelectorAll('.glide1').length) {
 
